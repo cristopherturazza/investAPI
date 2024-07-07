55dcbd1542f4692d1ef345ec11226d440af318b6
cristopherturazza <cristopherturazza@gmail.com>
2024-07-07 20:23:36 +0200

    refactor

--- src/routes/authorization.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import authController from "../controllers/authController";
-
-const router = Router();
-
-router.get("/", authController);
-
-export default router;
