4c9340da6c8048e8c09d7a175230398841155512
cristopherturazza <cristopherturazza@gmail.com>
2024-07-07 22:03:40 +0200

    manage basic authentication auth

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,13 +1,43 @@
-import generateToken, { Rights } from "../utils/generateToken";
+import { generateToken, Rights } from "../utils/generateToken";
+import { decodeBasicAuth, Credentials } from "../utils/decodeBasicAuth";
+import User from "../models/User";
 import { RequestHandler } from "express";
 
-const authController: RequestHandler = (req, res, next) => {
+const authController: RequestHandler = async (req, res, next) => {
   try {
-    const payload = {
-      rights: Rights.Read, // TODO: ReadWrite if basic authentication
-    };
-    const token = generateToken(payload);
-    return res.json({ token: token });
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+      // send token with read only rights
+      const payload = {
+        rights: Rights.Read,
+      };
+      const token = generateToken(payload);
+      return res.status(200).json({ token: token });
+    } else {
+      // send token with read and write rights
+      const credentials: Credentials | null = decodeBasicAuth(authorization);
+      if (!credentials) {
+        res.status(401);
+        return next(new Error("Invalid authorization header."));
+      }
+      const user = await User.checkCredentials(
+        credentials.username,
+        credentials.password
+      );
+
+      if (!user) {
+        res.status(401);
+        return next(new Error("User not found or invalid credentials."));
+      }
+
+      const payload = {
+        rights: Rights.ReadWrite,
+      };
+
+      const token = generateToken(payload);
+      return res.status(200).json({ token: token });
+    }
   } catch (error) {
     next(error);
   }
