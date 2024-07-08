56c4437e9e898fb88e6213fb3826f288cbb6f2a6
cristopherturazza <cristopherturazza@gmail.com>
2024-07-08 22:12:15 +0200

    change default expire

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -12,7 +12,7 @@ export interface jwtPayload extends jwt.JwtPayload {
 
 export const generateToken = (payload: jwtPayload) => {
   const secretKey: string = process.env.JWT_SECRET_PHRASE?.toString() || "";
-  const options = { expiresIn: process.env.JWT_EXPIRES?.toString() || "24h" };
+  const options = { expiresIn: process.env.JWT_EXPIRES?.toString() || "1h" };
 
   if (!secretKey) {
     throw new Error("JWT_SECRET_PHRASE is not defined");
